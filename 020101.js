743953ed1b4bb1bfe78b841c140082880dd8b5c8
AdTekDev <nguyenngtu777@gmail.com>
2021-10-08 15:44:44 +0700

    0

---
+++ 020101.js
@@ -0,0 +1,11 @@
+
+console.log("i  am first");
+
+setTimeout(
+    () => {
+        console.log("i am second");
+    },
+    5000
+);
+
+console.log("i  am third ");
---
+++ vd01.js
@@ -0,0 +1,5 @@
+
+x = 5;
+y = 7;
+
+console.log("KQ Tong=", x+y);
